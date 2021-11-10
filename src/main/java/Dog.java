f5acbcfa2c2f9bb46bf437cf13c3ab686b1752d5
Zara <24zalam@athenian.org>
2021-11-10 06:45:25 -0800

    done

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,35 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String breed;
 
 
     // Constructors
-    
+    public Dog(String a, int b, String c) {
+        super(a,b);
+        this.breed=c;
+    }
+    public Dog(String a, int b) {
+        super(a,b);
+        this.breed="Husky";
+
+    }
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Ruff!");
+    }
 
     // toString method
-
+    public String toString() {
+        String str = "Name: " + getName()+ ", Age: " + getAge() +"Breed:" + this.breed;
+        return str;
+    }
 
     // Getter
-
+public String getBreed() { return this.breed; }
 
     // Setter
+    public void setBreed(int classYear) {
+        this.breed = breed;
+    }
 }
